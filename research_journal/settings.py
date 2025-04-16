b6584a30b133b25e048aeee47c936a522b9e0af2
Mithilesh Lohar <128236295+mithlohar144@users.noreply.github.com>
2025-04-16 14:46:40 +0530

    Prepare for deployment

--- research_journal/settings.py
+++ research_journal/settings.py
@@ -28,19 +28,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-# Production settings
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Production settings
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
